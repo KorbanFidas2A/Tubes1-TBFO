7ba69e10a7006d7550b5396fbe546b1b67c92299
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2021-11-24 18:41:13 +0700

    fix cfg to cnf

--- cfg_to_cnf.py
+++ cfg_to_cnf.py
@@ -1,159 +1,202 @@
 import sys, itertools
 
-#file berisi grammar / syntax python
-variabel = []
-file = open('cfg.txt', 'r')
 
-#membaca file tersebut kemudian diubah formatnya menuju format python
-terminal = []
-bacagrammar = file.readlines()
-for i in range(len(bacagrammar)):
-    bacagrammar[i] = bacagrammar[i].replace('\n', '')
+def baca_grammar():
+    """ 
+    Mengubah CFG menjadi array
 
-#prod_res berarti adalah array untuk menampung hasil produksi, [Kiri] -> [Kanan] sudah dipisahkan
-prod_res = []
-for i in range(len(bacagrammar)):
-    if (bacagrammar[i][0] == '#'):
-        continue
-    left_side = bacagrammar[i].split(' -> ')[0]
-    if (left_side[0] >= 'A' and left_side[0] <= 'Z' and left_side not in variabel):
-        variabel.append(left_side)
-    right_side = bacagrammar[i].split(' -> ')[1].split(' | ')
-    for j in range(len(right_side)):
-        Terms = right_side[j].split(' ')
-        for term in Terms:
-            if (term >= 'a' and term <= 'z'):
-                if(term not in terminal):
-                    terminal.append(term)
-        prod_res.append((left_side, Terms))
+    """
+    file = open('cfg.txt', 'r')
+    bacagrammar = file.readlines()
+    for i in range(len(bacagrammar)):
+        bacagrammar[i] = bacagrammar[i].replace('\n', '')
+    return bacagrammar
 
-#Untuk menambahkan Sn kedalam variabel, untuk memulai Unit Production Elimination
-if (not 'Sn' in variabel):
-    variabel.append('Sn')
-    prod_res = [('Sn', [variabel[0]])] + prod_res
 
-#Membuat dictionary untuk terminal dan variabel
-#Seperti Hash Table, untuk mengakses seluruh variabel yang memiliki terminal tunggal di sebelah kanan
-#contoh: Y -> a , ALGORITMA -> baris baru
-dict = {}
+def pecah_array(bacagrammar, var, hasilprod, terms):
+    """
+    Memisahkan array di sebelah kiri dan kanan
 
-for product in prod_res:
-    if ((product[0] in variabel) and (product[1][0] in terminal) and (len(product[1]) == 1)):
-        dict[product[1][0]] = product[0]
+    """
+    for i in range(len(bacagrammar)):
+        if (bacagrammar[i][0] == '#'):
+            continue
+        left_side = bacagrammar[i].split(' -> ')[0]
+        if (left_side[0] >= 'A' and left_side[0] <= 'Z' and left_side not in var):
+            var.append(left_side)
+        right_side = bacagrammar[i].split(' -> ')[1].split(' | ')
+        for j in range(len(right_side)):
+            Terms = right_side[j].split(' ')
+            for term in Terms:
+                if (term >= 'a' and term <= 'z'):
+                    if(term not in terms):
+                        terms.append(term)
+            hasilprod.append((left_side, Terms))
 
-prod_res2 = []
-#Fungsi untuk membuat sebuah variabel baru -> formatnya
-#A[angka] -> (ekspresi)
-def create_new_var():
+def add_terminal(dictionary, hasilprod, var, terms):
+    """
+    Membuat hash table untuk mengakses terminal menggunakan dictionary
+    
+    """
+    for product in hasilprod:
+        if ((product[0] in var) and (product[1][0] in terms) and (len(product[1]) == 1)):
+            dictionary[product[1][0]] = product[0]
+
+def create_new_var(var2):
+    """
+    Membuat variabel baru
+
+    """
     i = 1
     var = 'A'
     variabel_baru = var.upper() + str(i)
-    while (variabel_baru in variabel):
+    while (variabel_baru in var2):
         i = i + 1
         variabel_baru = var.upper() + str(i)
     return variabel_baru
 
-#List untuk menyimpan hasil produksi (prod_res)
-#product[0] menyatakan bagian kiri, product[1] menyatakan bagian kanan, [kiri] -> [kanan]
-for product in prod_res:
-    #ini untuk memitigasi adanya yang hanya terminal A -> [terminal]
-    if ((product[0] in variabel) and (product[1][0] in terminal) and (len(product[1]) == 1)):
-        prod_res2.append(product)
-        #misalnya ada A -> B a
-    else:
-        for Terminal in terminal:
-            i = 0
-            for Term in product[1]: #mengecek di dalam A -> (A,B) product[1] menandakan A,B
-                #mencari apakah terminal terdapat pada bagian kiri sehingga misalnya A -> [terminal][variabel]
-                if ((Terminal == Term) and (not Terminal in dict)):
-                    #apabila ada maka, akan ditambahkan ke dalam dictionary. A -> a sebagai contoh.
-                    dict[Terminal] = create_new_var()
-                    variabel.append(dict[Terminal])
-                    prod_res2.append((dict[Terminal], [Terminal]))
-                    product[1][i] = dict[Terminal]
-                elif (Terminal == Term):
-                    product[1][i] = dict[Terminal]
-                i = i + 1
-        prod_res2.append((product[0], product[1]))
-        
-prod_res = prod_res2
+def change_var(hasilprod, newprod, var, term, dictionary):
+    """
+    mengganti terminal menjadi variabel
 
-#fungsi untuk mengeliminasi sebelah kanan (Sn -> [sebelah kanan]) yang memiliki variabel lebih dari 2
+    """
+    for product in hasilprod:
+        if ((product[0] in var) and (product[1][0] in term) and (len(product[1]) == 1)):
+            newprod.append(product)
+        else:
+            for Terminal in term:
+                i = 0
+                for Term in product[1]:
+                    if ((Terminal == Term) and (not Terminal in dictionary)):
+                        dictionary[Terminal] = create_new_var(var)
+                        var.append(dictionary[Terminal])
+                        newprod.append((dictionary[Terminal], [Terminal]))
+                        product[1][i] = dictionary[Terminal]
+                    elif (Terminal == Term):
+                        product[1][i] = dictionary[Terminal]
+                    i = i + 1
+            newprod.append((product[0], product[1]))
+    return newprod
+
+def eliminate_two_var(hasilprod, newprod, var):
+    """
+    untuk mengeliminasi variabel menjadi 2 variabel, maksimum
+
+    """
+    for product in hasilprod:
+        if (len(product[1]) <= 2):
+            newprod.append(product)
+        else:
+            newvar = create_new_var(var)
+            var.append(newvar)
+            newprod.append((product[0],[product[1][0]] + [newvar]))
+            newvar2 = newvar
+            newvar3 = create_new_var(var)
+            for i in range(1, len(product[1]) - 2):
+                var.append(newvar3)
+                newprod.append((newvar2, [product[1][i], newvar3]))
+                newvar2 = newvar3
+                newvar3 = create_new_var(var)
+            newprod.append((newvar2, product[1][len(product[1])-2:len(product[1])]))
+    return newprod
+
+def unit_production_elim(hasilprod, var):
+    """
+    melakukan unit production elimination
+
+    """
+    for i in range(500): 
+        prod_res2 = []
+        elim_unit = []
+        for product in hasilprod:
+            if ((product[0] in var) and (product[1][0] in var) and (len(product[1]) == 1)):
+                elim_unit.append((product[0], product[1][0]))
+            else:
+                prod_res2.append(product)
+        for units in elim_unit:
+            for product in hasilprod:
+                if ((units[1] == product[0]) and (units[0] != product[0])):
+                    prod_res2.append((units[0], product[1]))
+        hasilprod = prod_res2
+    return hasilprod
+
+def write_to_cnf(hasilprod):
+    """
+    menulis hasil terjemahan CFG ke CNF dalam txt
+
+    """
+    file = open('cnf.txt', 'w')
+    final = []
+
+    hasilprod = sorted(hasilprod)
+    for product in hasilprod:
+        if (product[0] == 'Sn'):
+            if (product[0] in final):
+                if (len(product[1]) == 1):
+                    file.write(' | ' + product[1][0])
+                else:
+                    file.write(' | ' + product[1][0] + ' ' + product[1][1])
+            else:
+                if (product == 'Sn'):
+                    file.write('\n')
+                final.append(product[0])
+                file.write(product[0] + ' -> ')
+                if (len(product[1]) == 1):
+                    file.write(product[1][0])
+                else:
+                    file.write(product[1][0] + ' ' + product[1][1])
+    for product in hasilprod:
+        if (product[0] != 'Sn'):
+            if (product[0] in final):
+                if (len(product[1]) == 1):
+                    file.write(' | ' + product[1][0])
+                else:
+                    file.write(' | ' + product[1][0] + ' ' + product[1][1])
+            else:
+                if (product != 'Sn'):
+                    file.write('\n')
+                final.append(product[0])
+                file.write(product[0] + ' -> ')
+                if (len(product[1]) == 1):
+                    file.write(product[1][0])
+                else:
+                    file.write(product[1][0] + ' ' + product[1][1])
+
+#MAIN PROGRAM
+
+variabel = []
+array_cfg = []
+
+#mengubah CFG menjadi array
+array_cfg = baca_grammar()
+
+#Memisahkan array di sebelah kiri dan kanan
+prod_res = []
+terminal = []
+pecah_array(array_cfg, variabel, prod_res, terminal)
+
+#menambahkan Sn -> S
+if (not 'Sn' in variabel):
+    variabel.append('Sn')
+    prod_res = [('Sn', [variabel[0]])] + prod_res
+
+dict = {}
+add_terminal(dict, prod_res, variabel, terminal)
+
+#mengganti terminal menjadi variabel
 prod_res2 = []
-for product in prod_res:
-    if (len(product[1]) <= 2):
-        prod_res2.append(product)
-    else:
-        #misalnya ada lebih dari 2
-        newvar = create_new_var()
-        variabel.append(newvar)
-        prod_res2.append((product[0],[product[1][0]] + [newvar]))
-        #membuat belakangnya itu jadi suatu variabel
-        newvar2 = newvar
-        newvar3 = create_new_var()
-        for i in range(1, len(product[1]) - 2):
-            variabel.append(newvar3)
-            prod_res2.append((newvar2, [product[1][i], newvar3]))
-            newvar2 = newvar3
-            newvar3 = create_new_var()
-        prod_res2.append((newvar2, product[1][len(product[1])-2:len(product[1])]))
+prod_res = change_var(prod_res, prod_res2, variabel, terminal, dict)
 
-prod_res = prod_res2
+#untuk mengeliminasi variabel menjadi 2 variabel, maksimum
+prod_res2 = []
+prod_res = eliminate_two_var(prod_res, prod_res2, variabel)
 
+#melakukan unit production elimination
+prod_res = unit_production_elim(prod_res, variabel)
 
-#Bagian untuk mengeliminasi unit production
-for i in range(500): 
-    prod_res2 = []
-    elim_unit = []
-    for product in prod_res:
-        if ((product[0] in variabel) and (product[1][0] in variabel) and (len(product[1]) == 1)):    # (A -> B form)
-            elim_unit.append((product[0], product[1][0]))
-        else:
-            prod_res2.append(product)
-    for units in elim_unit:
-        for product in prod_res:
-            if ((units[1] == product[0]) and (units[0] != product[0])):
-                prod_res2.append((units[0], product[1]))
-    prod_res = prod_res2
+#menulis hasil terjemahan CFG ke CNF dalam txt
+write_to_cnf(prod_res)
 
-#Membuka file cnf sehingga python akan mengetikkan hasil translasi
-file = open('cnf.txt', 'w')
-final = []
-
-#Hasil CFG menjadi CNF, Sn (Snew) sebagai S yang baru
-prod_res = sorted(prod_res)
-for product in prod_res:
-    if (product[0] == 'Sn'):
-        if (product[0] in final):
-            if (len(product[1]) == 1):
-                file.write(' | ' + product[1][0])
-            else:
-                file.write(' | ' + product[1][0] + ' ' + product[1][1])
-        else:
-            if (product == 'Sn'):
-                file.write('\n')
-            final.append(product[0])
-            file.write(product[0] + ' -> ')
-            if (len(product[1]) == 1):
-                file.write(product[1][0])
-            else:
-                file.write(product[1][0] + ' ' + product[1][1])
-for product in prod_res:
-    if (product[0] != 'Sn'):
-        if (product[0] in final):
-            if (len(product[1]) == 1):
-                file.write(' | ' + product[1][0])
-            else:
-                file.write(' | ' + product[1][0] + ' ' + product[1][1])
-        else:
-            if (product != 'Sn'):
-                file.write('\n')
-            final.append(product[0])
-            file.write(product[0] + ' -> ')
-            if (len(product[1]) == 1):
-                file.write(product[1][0])
-            else:
-                file.write(product[1][0] + ' ' + product[1][1])
 
 #Note: tidak ada eliminasi menggunakan epsilon
 #Pada grammar CFG tidak digunakan epsilon
